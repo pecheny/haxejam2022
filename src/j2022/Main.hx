64d6ff160956cf71f1aa0d6043fa7815609e1c16
artem pecheny <pecheny@gmail.com>
2022-07-29 14:03:56 +0300

    Stage scale/align.

--- src/j2022/Main.hx
+++ src/j2022/Main.hx
@@ -1,10 +1,14 @@
 package j2022;
+import openfl.display.StageAlign;
+import openfl.display.StageScaleMode;
 import utils.AbstractEngine;
 import j2022.GameFsm;
 class Main extends AbstractEngine {
     var fsm : GameFsm;
     public function new() {
         super();
+        stage.scaleMode = StageScaleMode.SHOW_ALL;
+        stage.align = StageAlign.TOP_LEFT;
         var m = new GodModel();
         fsm = new GameFsm(m);
         fsm.changeState(GameStates.INTRO);
