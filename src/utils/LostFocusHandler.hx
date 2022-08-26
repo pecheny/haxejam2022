7d9f5ad32ea497ab301208f4278754273f9e3c39
artem pecheny <pecheny@gmail.com>
2022-08-26 14:38:04 +0300

    Minor isolate platform-specific code under ifdef.

--- src/utils/LostFocusHandler.hx
+++ src/utils/LostFocusHandler.hx
@@ -5,10 +5,12 @@ class LostFocusHandler {
     public var onHide = new Signal<Void -> Void>();
 
     public function new() {
+        #if js
         js.Browser.document.addEventListener("visibilitychange", () -> {
             if (js.Browser.document.hidden)
                 fire(null);
         });
+        #end
 
 //        openfl.Lib.current.stage.addEventListener(FocusEvent.FOCUS_OUT, focusLost);
         openfl.Lib.current.stage.addEventListener(Event.DEACTIVATE, fire);
